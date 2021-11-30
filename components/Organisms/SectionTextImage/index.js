c42141ec6ee69f6c115847127220e59bc7c0ca30
daniellopeswt <80525257+daniellopeswt@users.noreply.github.com>
2021-11-30 19:46:59 +0000

    Update index.js

--- components/Organisms/SectionTextImage/index.js
+++ components/Organisms/SectionTextImage/index.js
@@ -2,17 +2,17 @@ import Image from "@/components/Atoms/Image";
 import dynamic from "next/dynamic";
 
 const TextImageCopy = dynamic(() =>
-  import("@/components/Molecules/textImageCopy").then((c) => c.TextImageCopy)
+  import("@/components/Molecules/TextImageCopy").then((c) => c.TextImageCopy)
 );
 
 export default function SectionTextImage(data) {
   return (
     <section className="relative pt-12 bg-blueGray-50">
       <div className="grid grid-cols-2 gap-10">
-        <div className="col-span-1">
+        <div className="col-span-2 lg:col-span-1">
           <Image classNameName={``} data={data.image} />
         </div>
-        <div className="col-span-1">
+        <div className="col-span-2 md:col-span-1">
           <TextImageCopy data={data} />
         </div>
       </div>
@@ -32,7 +32,7 @@ export const fragment = `
       addHeader
       content
       image {
-        responsiveImage(imgixParams: {crop: focalpoint, auto:format, q:60, w: "800", ar: "1:1"}) {
+        responsiveImage(imgixParams: {crop: focalpoint, auto:format, q:60, w: "800"}) {
             src
         }
     }
