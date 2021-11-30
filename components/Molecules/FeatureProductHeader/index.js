54dc0be734784bd0e912c632e3a63e664163d15b
daniellopeswt <80525257+daniellopeswt@users.noreply.github.com>
2021-11-30 21:58:52 +0000

    featureProduct update

---
+++ components/Molecules/FeatureProductHeader/index.js
@@ -0,0 +1,30 @@
+import dynamic from "next/dynamic";
+
+const HeadlineH2 = dynamic(() =>
+  import("@/components/Atoms/Title").then((c) => c.HeadlineH2)
+);
+
+const Subtitle = dynamic(() =>
+  import("@/components/Atoms/Subtitle").then((c) => c.Subtitle)
+);
+
+const Paragraph = dynamic(() =>
+  import("@/components/Atoms/Paragraph").then((c) => c.Paragraph)
+);
+
+export function FeatureProductHeader(props) {
+  return (
+    <>
+      <div className="lg:w-6/12 lg:p-0 p-7">
+        <HeadlineH2
+          className="text-4xl font-bold leading-tight mb-5 capitalize"
+          copy={props.data.title}
+        />
+        {props.data.addHeader && (
+          <Subtitle className="text-xl" copy={props.data.subtitle} />
+        )}
+        {props.data.content && <Paragraph copy={props.data.content} />}
+      </div>
+    </>
+  );
+}
--- components/Organisms/FeatureProduct/index.js
+++ components/Organisms/FeatureProduct/index.js
@@ -1,4 +1,11 @@
 import Image from "@/components/Atoms/Image";
+import dynamic from "next/dynamic";
+
+const FeatureProductHeader = dynamic(() =>
+  import("@/components/Molecules/FeatureProductHeader").then(
+    (c) => c.FeatureProductHeader
+  )
+);
 
 export default function sectionFeatureProduct(data) {
   return (
@@ -6,32 +13,7 @@ export default function sectionFeatureProduct(data) {
       <div className="py-16">
         <div className="container m-auto px-6">
           <div className="lg:flex justify-between items-center">
-            <div className="lg:w-6/12 lg:p-0 p-7">
-              <h2 className="text-4xl font-bold leading-tight mb-5 capitalize">
-                {" "}
-                Professional Tailwind theme designed for developers.{" "}
-              </h2>
-              <p className="text-xl">
-                {" "}
-                With Tailwind you can optimized the customization process to
-                save your team time when building websites.{" "}
-              </p>
-
-              <div className="py-5">
-                <a
-                  href="#"
-                  className="text-white rounded-full py-2 px-5 text-lg font-semibold bg-purple-600 inline-block border border-purple-600 mr-3"
-                >
-                  Try for free
-                </a>
-                <a
-                  href="#"
-                  className="text-black rounded-full py-2 px-5 text-lg font-semibold bg-gray-400 inline-block border hover:bg-white hover:text-black"
-                >
-                  Requist a demo
-                </a>
-              </div>
-            </div>
+            <FeatureProductHeader data={data} />
             {data.product &&
               data.product.media.map((entry) => (
                 <div className="order-2">
