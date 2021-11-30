b7502bbaa12e3b35b5c064fabe4459dbacab9bb9
daniellopeswt <80525257+daniellopeswt@users.noreply.github.com>
2021-11-30 22:37:44 +0000

    responsiveImageFragment added

--- components/Organisms/Logotypes/index.js
+++ components/Organisms/Logotypes/index.js
@@ -4,17 +4,21 @@ const LogotypeCard = dynamic(() =>
   import("@/components/Molecules/LogotypeCard").then((c) => c.LogotypeCard)
 );
 
-const SubSectionHeader  = dynamic(() =>
-  import("@/components/Molecules/SubSectionHeader").then((c) => c.SubSectionHeader)
+const SubSectionHeader = dynamic(() =>
+  import("@/components/Molecules/SubSectionHeader").then(
+    (c) => c.SubSectionHeader
+  )
 );
 
 export default function SectionLogotype(data) {
   return (
     <>
-    <SubSectionHeader data={data} />
+      <SubSectionHeader data={data} />
       <div className="sm:flex flex-wrap justify-center items-center text-center gap-8 ">
         {data.products &&
-          data.products.map((entry) => <LogotypeCard key={entry.id} data={entry} />)}
+          data.products.map((entry) => (
+            <LogotypeCard key={entry.id} data={entry} />
+          ))}
       </div>
     </>
   );
