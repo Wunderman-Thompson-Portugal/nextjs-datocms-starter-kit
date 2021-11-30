87bc2e6dc33a1dd75e418ae21948511782162a8d
Alison Silva <alison.silva@wundermanthompson.com>
2021-11-30 10:22:45 +0000

    svg

--- components/Templates/Main/Header/index.js
+++ components/Templates/Main/Header/index.js
@@ -5,34 +5,34 @@ export default function GenericHeader({ children }) {
         <nav>
             <div className={`max-w-6xl mx-auto px-4`}>
                 <div className={`flex justify-between`}>
-  
+
                     <div className={`flex space-x-4`}>
 
                         {/* LOGO */}
                         <div>
-                        <a href="#" className={`flex items-center py-5 px-2 text-gray-700 hover:text-gray-900`}>
-                            <svg className={`h-6 w-6 mr-1 text-blue-400`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinejoin="round" strokeLinejoin="round" strokeWidth="2" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8" />{" "}
-                            </svg>
-                            <span className={`font-bold`}>Wunderman Thompson Portugal</span>
-                        </a>
+                            <a href="#" className={`flex items-center py-5 px-2 text-gray-700 hover:text-gray-900`}>
+                                <svg className={`h-6 w-6 mr-1 text-blue-400`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinejoin="round" strokeLinejoin="round" strokeWidth="2" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8" />
+                                </svg>
+                                <span className={`font-bold`}>Wunderman Thompson Portugal</span>
+                            </a>
                         </div>
 
                         {/* PRIMARY NAV */}
                         <div className={`hidden md:flex items-center space-x-1`}>
                             <a href="#" className={`py-5 px-3 text-gray-700 hover:text-gray-900`}>Features</a>
                             <a href="#" className={`py-5 px-3 text-gray-700 hover:text-gray-900`}>Pricing</a>
-                            </div>
                         </div>
+                    </div>
 
-                        {/* mobile button goes here */}
-                        <div className={`md:hidden flex items-center`}>
+                    {/* mobile button goes here */}
+                    <div className={`md:hidden flex items-center`}>
                         <button className={`mobile-menu-button`}>
                             <svg className={`w-6 h-6`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinejoin="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />{" "}
+                                <path strokeLinejoin="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
                             </svg>
                         </button>
-                        </div>
+                    </div>
 
                 </div>
             </div>
