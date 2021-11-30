ab59f3e8a32bd6d4ecc5d26cb12de6c96bd671ea
Rodrigo Barona <rbarona@gmail.com>
2021-11-30 19:32:01 +0000

    Add Markdown and some class to Hero Image

--- components/Organisms/Hero/HeroImage/index.js
+++ components/Organisms/Hero/HeroImage/index.js
@@ -1,5 +1,5 @@
 import styles from "./index.module.scss";
-
+import ReactMarkdown from "react-markdown";
 import Image from "@/components/Atoms/Image";
 import { AppleIcon, AndroidIcon } from "@/components/Atoms/Vectors";
 
@@ -8,49 +8,26 @@ export default function HeroImage(data) {
   return (
     <>
       {/* hero */}
-      <div className="hero bg-gray-100 py-16">
+      <div className="hero bg-gray-100">
         {/* container */}
-        <div className="container px-4 sm:px-8 lg:px-16 xl:px-20 mx-auto">
+        <div className="container pt-8 md:pt-0 pl-0 sm:pl-8 lg:pl-16 xl:pl-20 mx-auto">
           {/* hero wrapper */}
           <div className="hero-wrapper grid grid-cols-1 md:grid-cols-12 gap-8 items-center">
             {/* hero text */}
-            <div className="hero-text col-span-6">
-              <h1 className=" font-bold text-4xl md:text-5xl max-w-xl text-gray-900 leading-tight">
+            <div className="hero-text col-span-6  px-4 md:px-0">
+              <h1 className=" font-bold text-4xl md:text-5xl max-w-xl text-gray-900 leading-tight mb-6">
                 {data.title}
               </h1>
-              <p className="text-gray-800 text-base leading-relaxed mt-8 font-semibold">
+              <ReactMarkdown className="prose prose-sm max-w-none">  
                 {data.description}
-              </p>
-              <div className="get-app flex space-x-5 mt-10 justify-center md:justify-start">
-                <button className="apple bg-white shadow-md px-3 py-2 rounded-lg flex items-center space-x-4">
-                  <div className="logo">
-                    <AppleIcon />
-                  </div>
-                  <div className="text">
-                    <p className=" text-xs text-gray-600">Download on the</p>
-                    <p className=" text-xs font-semibold text-gray-900">
-                      Apple Store
-                    </p>
-                  </div>
-                </button>
-                <button className="google bg-white shadow-md px-3 py-2 rounded-lg flex items-center space-x-4">
-                  <div className="image">
-                    <AndroidIcon />
-                  </div>
-                  <div className="text">
-                    <p className="text-xs text-gray-600">Download it from</p>
-                    <p className="text-xs font-semibold text-gray-900">
-                      Google play
-                    </p>
-                  </div>
-                </button>
-              </div>
+              </ReactMarkdown>
+              
             </div>
 
             {/* hero image */}
-            <div className="relative hero-image col-span-6 z-10 min-h-[30rem]">
+            <div className="relative hero-image col-span-6 z-10 min-h-[15rem] md:min-h-[50vh]">
               <Image
-                className={`min-h-[30rem] w-full ${styles.overlayBg}`}
+                className={`min-h-[15rem] md:min-h-[50vh] w-full ${styles.overlayBg}`}
                 data={data.image}
               />
             </div>
@@ -69,7 +46,7 @@ export const fragment = `
         title
         description
         image {
-            responsiveImage(imgixParams: {fit: crop, crop: focalpoint, auto:format, q:60, w: "1200", ar: "2:1"}) {
+            responsiveImage(imgixParams: {fit: crop, crop: focalpoint, auto:format, q:60, w: "1200", ar: "1:1"}) {
                 src
             }
         }
