47cf84ab6bfaa5e720745cb91ca32b7144796f86
daniellopeswt <80525257+daniellopeswt@users.noreply.github.com>
2021-11-30 10:34:25 +0000

    svg prop duplicate fix

--- components/Templates/Main/Header/index.js
+++ components/Templates/Main/Header/index.js
@@ -12,7 +12,7 @@ export default function GenericHeader({ children }) {
                         <div>
                             <a href="#" className={`flex items-center py-5 px-2 text-gray-700 hover:text-gray-900`}>
                                 <svg className={`h-6 w-6 mr-1 text-blue-400`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path strokeLinejoin="round" strokeLinejoin="round" strokeWidth="2" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8" />
+                                    <path  strokeLinejoin="round" strokeWidth="2" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8" />
                                 </svg>
                                 <span className={`font-bold`}>Wunderman Thompson Portugal</span>
                             </a>
@@ -29,7 +29,7 @@ export default function GenericHeader({ children }) {
                     <div className={`md:hidden flex items-center`}>
                         <button className={`mobile-menu-button`}>
                             <svg className={`w-6 h-6`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path strokeLinejoin="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
+                                <path strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
                             </svg>
                         </button>
                     </div>
