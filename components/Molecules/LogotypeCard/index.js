7efc948f1aff1608171b37702ad81dfe97b11e70
daniellopeswt <80525257+daniellopeswt@users.noreply.github.com>
2021-11-30 21:40:54 +0000

    Update index.js

--- components/Molecules/LogotypeCard/index.js
+++ components/Molecules/LogotypeCard/index.js
@@ -11,10 +11,10 @@ export function LogotypeCard(props) {
       <div className="w-full sm:w-1/2 md:w-1/2 lg:w-1/4 px-4 pt-12 pb-4 bg-white mt-6  shadow-lg rounded-lg dark:bg-gray-800">
         <div className="flex-shrink-0">
           <div className="flex items-center mx-auto justify-center h-12 w-12 ">
-            <Image classNameName={`min-w-[5rem]`} data={props.data.logo} />
+            <Image className={`min-w-[5rem]`} data={props.data.logo} />
           </div>
         </div>
-        {props.data.description && <Paragraph classNameName="mt-12 text-sm" copy={props.data.description} />}
+        {props.data.description && <Paragraph className="mt-12 text-sm" copy={props.data.description} />}
       </div>
     </>
   );
