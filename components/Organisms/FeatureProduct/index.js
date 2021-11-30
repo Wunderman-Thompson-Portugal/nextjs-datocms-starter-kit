dff1205e90e6b0d66c4cb9e08b9e8e03d80de1b9
daniellopeswt <80525257+daniellopeswt@users.noreply.github.com>
2021-11-30 21:38:32 +0000

    Update index.js

--- components/Organisms/FeatureProduct/index.js
+++ components/Organisms/FeatureProduct/index.js
@@ -3,30 +3,30 @@ import Image from "@/components/Atoms/Image";
 export default function sectionFeatureProduct(data) {
   return (
     <>
-      <div class="py-16">
-        <div class="container m-auto px-6">
-          <div class="lg:flex justify-between items-center">
-            <div class="lg:w-6/12 lg:p-0 p-7">
-              <h2 class="text-4xl font-bold leading-tight mb-5 capitalize">
+      <div className="py-16">
+        <div className="container m-auto px-6">
+          <div className="lg:flex justify-between items-center">
+            <div className="lg:w-6/12 lg:p-0 p-7">
+              <h2 className="text-4xl font-bold leading-tight mb-5 capitalize">
                 {" "}
                 Professional Tailwind theme designed for developers.{" "}
               </h2>
-              <p class="text-xl">
+              <p className="text-xl">
                 {" "}
                 With Tailwind you can optimized the customization process to
                 save your team time when building websites.{" "}
               </p>
 
-              <div class="py-5">
+              <div className="py-5">
                 <a
                   href="#"
-                  class="text-white rounded-full py-2 px-5 text-lg font-semibold bg-purple-600 inline-block border border-purple-600 mr-3"
+                  className="text-white rounded-full py-2 px-5 text-lg font-semibold bg-purple-600 inline-block border border-purple-600 mr-3"
                 >
                   Try for free
                 </a>
                 <a
                   href="#"
-                  class="text-black rounded-full py-2 px-5 text-lg font-semibold bg-gray-400 inline-block border hover:bg-white hover:text-black"
+                  className="text-black rounded-full py-2 px-5 text-lg font-semibold bg-gray-400 inline-block border hover:bg-white hover:text-black"
                 >
                   Requist a demo
                 </a>
@@ -34,8 +34,8 @@ export default function sectionFeatureProduct(data) {
             </div>
             {data.product &&
               data.product.media.map((entry) => (
-                <div class="lg:w-5/12 order-2">
-                  <Image className={``} data={entry.image} />
+                <div className="order-2">
+                  <Image classNameName={``} data={entry.image} />
                 </div>
               ))}
           </div>
@@ -61,7 +61,7 @@ export const fragment = `
           ... on PictureRecord {
           id
           image {
-            responsiveImage {
+            responsiveImage(imgixParams: {fit: clamp, auto:format, q:60, w: "600", ar: "2:1"}) {
               src
             }
           }
