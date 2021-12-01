62a97dfe1d9c3fe1927184c3e166fa1019447c6f
Rodrigo Barona <rbarona@gmail.com>
2021-12-01 03:36:49 +0000

    leeft and right on Hero Image

--- components/Organisms/Hero/HeroImage/index.js
+++ components/Organisms/Hero/HeroImage/index.js
@@ -12,11 +12,21 @@ export default function HeroImage(data) {
       {/* hero */}
       <div className="hero bg-gray-200 rounded-xl overflow-hidden">
         {/* container */}
-        <div className="container pt-8 pl-0 md:pt-0 md:pl-8 lg:pl-16 xl:pl-20 max-w-full ">
+        <div
+          className={`${
+            data.alignment == "Left"
+              ? "md:pl-8 lg:pl-16 xl:pl-20"
+              : "md:pr-8 lg:pr-16 xl:pr-20"
+          } container pt-8 pl-0 md:pt-0   max-w-full`}
+        >
           {/* hero wrapper */}
-          <div className="hero-wrapper grid grid-cols-1 md:grid-cols-12 gap-8 items-center ">
+          <div className="hero-wrapper grid grid-cols-1 md:grid-cols-12 gap-20 items-center ">
             {/* hero text */}
-            <div className="hero-text col-span-6  px-4 md:px-0">
+            <div
+              className={`${
+                data.alignment == "Left" ? "order-1" : "order-2"
+              } hero-text col-span-6  px-4 md:px-0`}
+            >
               <h1 className=" font-sans font-bold  text-4xl md:text-5xl max-w-xl text-gray-900 leading-tight mb-6">
                 {data.title}
               </h1>
@@ -26,7 +36,10 @@ export default function HeroImage(data) {
             </div>
 
             {/* hero image */}
-            <div className="relative hero-image col-span-6 z-10 ">
+            <div
+              className={`${data.alignment == "Left" ? "order-2" : "order-1"}
+            relative hero-image col-span-6 z-10 `}
+            >
               <Image className={`w-full`} data={data.image} />
             </div>
           </div>
@@ -43,6 +56,7 @@ export const fragment = `
         id
         title
         description
+        alignment
         image {
             responsiveImage(imgixParams: {fit: crop, crop: focalpoint, auto:format, q:60, w: "1200", ar: "1:1"}) {
                 ...responsiveImageFragment
