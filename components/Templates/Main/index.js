ed642ccc5b2fd288a353a11e5c18015d194ee2cf
Alison Silva <alison.silva@wundermanthompson.com>
2021-12-01 12:34:06 +0000

    fix metadata null

--- components/Templates/Main/index.js
+++ components/Templates/Main/index.js
@@ -4,15 +4,15 @@ import Head from "next/head";
 
 export default function MainLayout({ children }) {
   const header = children.props?.siteConfig?.headerConf;
-  const pageMetaTags = children.props.data._seoMetaTags;
-  const siteMetaTags = children.props.siteConfig._seoMetaTags;
-  const siteTitle = siteMetaTags.filter(meta => meta.tag === 'title').map(meta => meta.content)
+  const pageMetaTags = children.props?.data._seoMetaTags;
+  const siteMetaTags = children.props?.siteConfig._seoMetaTags;
+  const siteTitle = siteMetaTags?.filter(meta => meta.tag === 'title')?.map(meta => meta.content)
 
   return (
     <>
       <Head>
         {
-          pageMetaTags.map((meta, key) => {
+          pageMetaTags?.map((meta, key) => {
             if (meta.tag === 'meta' && meta.attributes !== null) {
               return <meta key={key} name={meta.attributes.property} content={meta.attributes.content} />;
             } else if (meta.tag === 'title') {
