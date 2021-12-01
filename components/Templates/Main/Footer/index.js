b05586804fbce49b0843fd50e73714aef96abb9f
Rodrigo Barona <rbarona@gmail.com>
2021-12-01 23:56:21 +0000

    Update index.js

--- components/Templates/Main/Footer/index.js
+++ components/Templates/Main/Footer/index.js
@@ -1,16 +1,17 @@
 import { useRouter } from "next/router";
 
 export default function GenericFooter({ children }) {
-    const { route } = useRouter();
+  const { route } = useRouter();
 
-    if (route === '/')
-        return <></>;
+  if (route === "/") return <></>;
 
-    return (
-        <footer className={`bg-gray-500 p-6`}>
-            <div className={`max-w-6xl mx-auto px-4`}>
-                <p className={`text-white text-sm mt-48 mb-4`}>Copyright © 2021 | All rights reserved</p>
-            </div>
-        </footer>
-    )
+  return (
+    <footer className={`bg-gray-500 p-6`}>
+      <div className={`max-w-6xl mx-auto px-4`}>
+        <p className={`text-white prose-sm text-sm mt-48 mb-4`}>
+          Copyright © 2021 | All rights reserved
+        </p>
+      </div>
+    </footer>
+  );
 }
